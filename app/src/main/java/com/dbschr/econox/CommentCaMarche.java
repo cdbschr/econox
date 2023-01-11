05b325126bd26abb35ad399c45f7b10996483102
Camille Dbschr <c.debusscher@gmail.com>
2023-01-11 09:22:37 +0100

    add: first commit, architecture

---
+++ app/src/main/java/com/dbschr/econox/CommentCaMarche.java
@@ -0,0 +1,14 @@
+package com.dbschr.econox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommentCaMarche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment_ca_marche);
+    }
+}
---
+++ app/src/main/java/com/dbschr/econox/Lieu.java
@@ -0,0 +1,14 @@
+package com.dbschr.econox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lieu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lieu);
+    }
+}
